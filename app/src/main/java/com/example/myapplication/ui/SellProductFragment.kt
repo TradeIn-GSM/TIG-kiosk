6d36f0735cc511b111f851699fe271d36550fb9e
류호성 <52982845+Hoseong-Ryu@users.noreply.github.com>
2021-01-06 18:43:11 +0900

    backToHome

--- app/src/main/java/com/example/myapplication/ui/SellProductFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/SellProductFragment.kt
@@ -1,10 +1,12 @@
 package com.example.myapplication.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.myapplication.R
 
 class SellProductFragment : Fragment() {
@@ -14,6 +16,12 @@ class SellProductFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val mHandler = Handler()
+        mHandler.postDelayed(Runnable {
+            findNavController().navigate(R.id.action_sellProductFragment_to_loginFragment)
+
+        }, 3000)
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sell_product, container, false)
     }
